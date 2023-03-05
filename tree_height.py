2d18797a6128bb3772e26baf4364b4a22e7aaf33
221RDC029 <125128863+221RDC029@users.noreply.github.com>
2023-03-05 12:21:04 +0000

    Test

--- tree_height.py
+++ tree_height.py
@@ -4,13 +4,14 @@ import sys
 import threading
 import numpy
 
-
 def compute_height(n, parents):
+    # Izveido masīvu, kur saglabāt vērtības
     koks = [[] for i in range(n)]
+    # Piešķir parent_garums vērtību - ja tas ir vienāds ar -1, tad tas būs sākums
     for i in range(n):
         parent_garums = parents[i]
         if parent_garums == -1:
-            root_garums = i
+            root_garums = -1
         else:
             koks[parent_garums].append(i)
 
@@ -27,23 +28,27 @@ def main():
     if "I" in ievade:
          # input number of elements
         n = int(input())
-        parents = list (map(int, input().split () ))
+    # input values in one variable, separate with space, split these values in an array
+        vertibas = list(map(int, input().split()))
     if "F" in ievade:
-        mape = './test/'
-        while True:
-            faila_nosaukums = input()
-            if "a" in faila_nosaukums:
-                print("Faila nosaukumā nevar būt burts 'a' ")
-            else:
-                break
-        vieta = mape + faila_nosaukums
-        with open(vieta, 'r') as f:
-            n = int(f.readline().strip())
-            parents = list(map(int, f.readline().strip().split()))
-    # let user input file name to use, don't allow file names with letter a
+        faila_nosaukums = input()
+        path = 'test/'
+        # let user input file name to use, don't allow file names with letter a
+        if "a" in faila_nosaukums:
+            return
+        else:
+            # Nolasa vērtības no faila
+            try:
+                with open(path + faila_nosaukums, 'r') as file:
+                    n = int(file.readline())
+                    parents = list(map(int, file.readline().split()))
+                    
+            except Exception as ex:
+                print("Fails neeksiste", str(ex))
+                return
+
     # account for github input inprecision
 
-    # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
     result = compute_height(n, parents)
     print(result)
